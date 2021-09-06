531bc028c402f1eed90018370a2b717e951c79be
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2021-09-06 23:52:39 +0900

    次世代の実装

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ const App: Component = () => {
   return (
     <div>
       <h1>Game of Life!</h1>
+
+      <div class="control">
+        generation: {store.state.generation}
+        <button onClick={store.next}>next</button>
+      </div>
+
       <For each={store.state.cells}>
         {(row, ri) => (
           <div class="row">
--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,7 @@ import { createStore } from "solid-js/store";
 
 type State = {
   cells: boolean[][];
+  generation: number;
 };
 
 const WIDTH = 25;
@@ -9,6 +10,7 @@ const HEIGHT = 15;
 
 const [state, setState] = createStore<State>({
   cells: [...Array(HEIGHT)].map(() => [...Array(WIDTH)].fill(false)),
+  generation: 0,
 });
 
 export const store = {
@@ -16,4 +18,39 @@ export const store = {
   setCell: (ri: number, ci: number, value: boolean) => {
     setState("cells", ri, ci, value);
   },
+  next: () => {
+    setState("cells", makeNext());
+    setState("generation", (v) => v + 1);
+  },
 };
+
+const makeNext = () =>
+  state.cells.map((row, ri, cells) =>
+    row.map((col, ci) => {
+      let count = 0;
+
+      // 上の段
+      const upperRow = cells[ri - 1];
+      if (upperRow) {
+        if (upperRow[ci - 1]) count++;
+        if (upperRow[ci]) count++;
+        if (upperRow[ci + 1]) count++;
+      }
+      // 左右
+      if (row[ci - 1]) count++;
+      if (row[ci + 1]) count++;
+      // 下の段
+      const lowerRow = cells[ri + 1];
+      if (lowerRow) {
+        if (lowerRow[ci - 1]) count++;
+        if (lowerRow[ci]) count++;
+        if (lowerRow[ci + 1]) count++;
+      }
+
+      // if (!col && count === 3) return true;
+      // if (col && (count === 2 || count === 3)) return true;
+      // return false;
+
+      return count === 3 || (col && count === 2);
+    })
+  );
